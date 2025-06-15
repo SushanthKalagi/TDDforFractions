3641cdc322632e0c50196b3fd3d39f8107795699
Sushanth <Sushanth@DAC-0185.local>
2025-06-15 11:08:36 -0700

    Refactored test cases

--- src/test/java/org/example/FractionEqualsTest.java
+++ src/test/java/org/example/FractionEqualsTest.java
@@ -8,27 +8,27 @@ import static org.junit.Assert.assertNotEquals;
 public class FractionEqualsTest {
 
     @Test
-    public void sameNumeratorAndDenominator() throws Exception{
+    public void sameNumeratorAndDenominator() {
         assertEquals(new Fraction(3,5), new Fraction(3,5));
     }
 
     @Test
-    public void differentNumerators() throws Exception{
+    public void differentNumerators() {
         assertNotEquals(new Fraction(3,8), new Fraction(5,8));
     }
 
     @Test
-    public void differentDenominators() throws Exception{
+    public void differentDenominators() {
         assertNotEquals(new Fraction(3,8), new Fraction(3,9));
     }
 
     @Test
-    public void wholeNumberEqualsSameFraction() throws Exception{
+    public void wholeNumberEqualsSameFraction() {
         assertEquals(new Fraction(2,1), new Fraction(2,1));
     }
 
     @Test
-    public void wholeNumberNotEqualsDifferentWholeNumber() throws Exception{
+    public void wholeNumberNotEqualsDifferentWholeNumber() {
         assertNotEquals(new Fraction(2), new Fraction(3));
     }
 }
